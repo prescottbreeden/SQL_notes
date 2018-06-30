cf417165fc6aa7eb1af9c6d1e8340688652a8765
prescott breeden <prescott.breeden@gmail.com>
2018-06-30 15:22:34 -0700

    refactored directory structure

--- practice.sql
+++ practice.sql
@@ -1,33 +1,39 @@
+-- CREATE EMPLOYEES TABLE
+
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    last_name VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255),
-    age INTEGER NOT NULL,
-    current_status VARCHAR(100) NOT NULL DEFAULT 'employed'
+    employee_id		INTEGER			NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
+    first_name		VARCHAR(255)	NOT NULL,
+    last_name		VARCHAR(255)	NOT NULL,
+    middle_name		VARCHAR(255),
+    age				INTEGER			NOT NULL,
+    current_status	VARCHAR(100)	NOT NULL	DEFAULT 'employed',
+	created_date	DATETIME		NOT NULL	DEFAULT NOW(),
+	updated_date	DATETIME		NOT NULL	DEFAULT NOW()	ON UPDATE NOW()
 );
 
-INSERT INTO employees
-            (last_name, first_name, age, current_status)
-VALUES      ('Breeden', 'Prescott', 35, 'fired'),
-            ('Franklin', 'Bob', 62, 'retired');
+ INSERT INTO employees 
+		(last_name, first_name, age, current_status)
+ VALUES ('Breeden', 'Prescott', 35, 'fired'),
+        ('Franklin', 'Bob', 62, 'retired');
+
+-- CREATE SHIRTS TABLE
 
 CREATE TABLE shirts (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    article VARCHAR(50) NOT NULL,
-    color VARCHAR(50) NOT NULL,
-    shirt_size VARCHAR(3) NOT NULL,
-    last_worn INT NOT NULL DEFAULT 0
+    shirt_id		INTEGER			NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
+    article			VARCHAR(50)		NOT NULL,
+    color			VARCHAR(50)		NOT NULL,
+    shirt_size		VARCHAR(3)		NOT NULL,
+    last_worn		INTEGER			NOT NULL	DEFAULT 0,
+	created_date	DATETIME		NOT NULL	DEFAULT NOW(),
+	updated_date	DATETIME		NOT NULL	DEFAULT NOW()	ON UPDATE NOW()
 );
 
-INSERT INTO shirts
-    (article, color, shirt_size, last_worn)
-VALUES
-    ('t-shirt', 'white', 'S', 10),
-    ('t-shirt', 'green', 'S', 200),
-    ('polo-shirt', 'black', 'M', 10),
-    ('tank top', 'blue', 'S', 50),
-    ('t-shirt', 'pink', 'S', 0),
-    ('polo shirt', 'red', 'M', 5),
-    ('tank top', 'white', 'S', 200),
-    ('tank top', 'blue', 'M', 15);
+ INSERT INTO shirts (article, color, shirt_size, last_worn)
+ VALUES ('t-shirt', 'white', 'S', 10),
+		('t-shirt', 'green', 'S', 200),
+		('polo-shirt', 'black', 'M', 10),
+		('tank top', 'blue', 'S', 50),
+		('t-shirt', 'pink', 'S', 0),
+		('polo shirt', 'red', 'M', 5),
+		('tank top', 'white', 'S', 200),
+		('tank top', 'blue', 'M', 15);
--- users.sql
+++ users.sql
@@ -1,34 +1,10 @@
-CREATE TABLE users 
-	(
-		id INT AUTO_INCREMENT PRIMARY KEY,
-		first_name VARCHAR(50) NOT NULL,
-		last_name VARCHAR(50) NOT NULL,
-		email VARCHAR(100) NOT NULL,
-		password VARCHAR(100) NOT NULL,
-		salt VARCHAR(50) NOT NULL,
-		created_at DATETIME DEFAULT NOW(),
-		updated_at DATETIME DEFAULT NOW()
-	);
-
-CREATE TABLE posts
-	(
-		id INT AUTO_INCREMENT PRIMARY KEY,
-		content TEXT NOT NULL,
-		user_id INT NOT NULL,
-		created_at DATETIME DEFAULT NOW(),
-		updated_at DATETIME DEFAULT NOW(),
-		FOREIGN KEY(user_id) REFERENCES users(id)
-	);
-
-CREATE TABLE comments
-	(
-		id INT AUTO_INCREMENT PRIMARY KEY,
-		content TEXT NOT NULL,
-		user_id INT NOT NULL,
-		post_id INT NOT NULL,
-		created_at DATETIME DEFAULT NOW(),
-		updated_at DATETIME DEFAULT NOW(),
-		FOREIGN KEY(post_id) REFERENCES posts(id),
-		FOREIGN KEY(user_id) REFERENCES users(id)
-	);
+CREATE TABLE users (
+	user_id		INTEGER			NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
+	username		VARCHAR(100)	NOT NULL,
+	email			VARCHAR(100)	NOT NULL,
+	password		VARCHAR(100)	NOT NULL,
+	salt			CHAR(4), 
+	created_date	DATETIME		NOT NULL	DEFAULT NOW(),
+	updated_date	DATETIME		NOT NULL	DEFAULT NOW()	ON UPDATE NOW()
+);
 
